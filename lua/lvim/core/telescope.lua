c80d2d8b877153ec371d5066fe2398e7a4075e4a
lucioreiz <lucio.000@hotmail.com>
2022-12-30 17:35:19 -0300

    TASK: Added press q to quit telescope popup

--- lua/lvim/core/telescope.lua
+++ lua/lvim/core/telescope.lua
@@ -55,6 +55,7 @@ function M.config()
           ["<CR>"] = actions.select_default,
         },
         n = {
+          ["q"] = actions.close,
           ["<C-j>"] = actions.move_selection_next,
           ["<C-k>"] = actions.move_selection_previous,
           ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
