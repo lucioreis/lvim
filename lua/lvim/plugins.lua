1fc3a5ba22ff9398bfd75534ba57ce62c2e743b3
lucioreiz <lucio.000@hotmail.com>
2023-01-29 22:26:01 -0300

    TASK: Migrating to Lazy vim.[DONE]

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -1,11 +1,14 @@
 -- local require = require("lvim.utils.require").require
 local core_plugins = {
-  -- Packer can manage itself as an optional plugin
-  { "wbthomason/packer.nvim" },
-  { "neovim/nvim-lspconfig" },
-  { "tamago324/nlsp-settings.nvim" },
-  { "jose-elias-alvarez/null-ls.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
+  { "folke/lazy.nvim", tag = "stable" },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    dependencies = { "mason-lspconfig.nvim", "nlsp-settings.nvim" },
+  },
+  { "williamboman/mason-lspconfig.nvim", lazy = true },
+  { "tamago324/nlsp-settings.nvim", lazy = true },
+  { "jose-elias-alvarez/null-ls.nvim", lazy = true },
   {
     "williamboman/mason.nvim",
     config = function()
@@ -29,13 +32,15 @@ local core_plugins = {
     config = function()
       require("lvim.core.telescope").setup()
     end,
-    disable = not lvim.builtin.telescope.active,
+    dependencies = { "telescope-fzf-native.nvim" },
+    lazy = true,
+    cmd = "Telescope",
+    enabled = lvim.builtin.telescope.active,
   },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    requires = { "nvim-telescope/telescope.nvim" },
-    run = "make",
-    disable = not lvim.builtin.telescope.active,
+  { "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    lazy = true,
+    enabled = lvim.builtin.telescope.active
   },
   -- Install nvim-cmp, and buffer source as a dependency
   {
@@ -45,21 +50,25 @@ local core_plugins = {
         require("lvim.core.cmp").setup()
       end
     end,
-    requires = {
-      "L3MON4D3/LuaSnip",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      "cmp-nvim-lsp",
+      "cmp_luasnip",
+      "cmp-buffer",
+      "cmp-path",
     },
   },
-  {
-    "rafamadriz/friendly-snippets",
-    disable = not lvim.builtin.luasnip.sources.friendly_snippets,
-  },
+  { "hrsh7th/cmp-nvim-lsp", lazy = true },
+  { "saadparwaiz1/cmp_luasnip", lazy = true },
+  { "hrsh7th/cmp-buffer", lazy = true },
+  { "hrsh7th/cmp-path", lazy = true },
   {
     "L3MON4D3/LuaSnip",
     config = function()
       local utils = require "lvim.utils"
       local paths = {}
       if lvim.builtin.luasnip.sources.friendly_snippets then
-        paths[#paths + 1] = utils.join_paths(get_runtime_dir(), "site", "pack", "packer", "start", "friendly-snippets")
+        paths[#paths + 1] = utils.join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt", "friendly-snippets")
       end
       local user_snippets = utils.join_paths(get_config_dir(), "snippets")
       if utils.is_directory(user_snippets) then
@@ -71,32 +80,27 @@ local core_plugins = {
       }
       require("luasnip.loaders.from_snipmate").lazy_load()
     end,
+    event = "InsertEnter",
+    dependencies = {
+      "friendly-snippets",
+    },
   },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "saadparwaiz1/cmp_luasnip",
-  },
-  {
-    "hrsh7th/cmp-buffer",
-  },
-  {
-    "hrsh7th/cmp-path",
-  },
+  { "rafamadriz/friendly-snippets",
+    lazy = true,
+    cond = lvim.builtin.luasnip.sources.friendly_snippets },
   {
     "folke/neodev.nvim",
-    module = "neodev",
+    lazy = true
   },
 
   -- Autopairs
   {
     "windwp/nvim-autopairs",
-    -- event = "InsertEnter",
+    event = "InsertEnter",
     config = function()
       require("lvim.core.autopairs").setup()
     end,
-    disable = not lvim.builtin.autopairs.active,
+    enabled = lvim.builtin.autopairs.active,
   },
 
   -- Treesitter
@@ -109,7 +113,7 @@ local core_plugins = {
   },
   {
     "JoosepAlviste/nvim-ts-context-commentstring",
-    event = "BufReadPost",
+    event = "VeryLazy",
   },
 
   -- NvimTree
@@ -120,16 +124,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.nvimtree").setup()
     end,
-    disable = not lvim.builtin.nvimtree.active,
-  },
-  -- Lir
-  {
-    "christianchiarulli/lir.nvim",
-    config = function()
-      require("lvim.core.lir").setup()
-    end,
-    requires = { "kyazdani42/nvim-web-devicons" },
-    disable = not lvim.builtin.lir.active,
+    enabled = lvim.builtin.nvimtree.active,
   },
   {
     "lewis6991/gitsigns.nvim",
@@ -138,7 +133,7 @@ local core_plugins = {
       require("lvim.core.gitsigns").setup()
     end,
     event = "BufRead",
-    disable = not lvim.builtin.gitsigns.active,
+    enabled = lvim.builtin.gitsigns.active,
   },
 
   -- Whichkey
@@ -147,8 +142,8 @@ local core_plugins = {
     config = function()
       require("lvim.core.which-key").setup()
     end,
-    event = "BufWinEnter",
-    disable = not lvim.builtin.which_key.active,
+    event = "VeryLazy",
+    enabled = lvim.builtin.which_key.active,
   },
 
   -- Comments
@@ -158,7 +153,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.comment").setup()
     end,
-    disable = not lvim.builtin.comment.active,
+    enabled = lvim.builtin.comment.active,
   },
 
   -- project.nvim
@@ -167,14 +162,14 @@ local core_plugins = {
     config = function()
       require("lvim.core.project").setup()
     end,
-    disable = not lvim.builtin.project.active,
+    enabled = lvim.builtin.project.active,
   },
 
   -- Icons
-  {
-    "kyazdani42/nvim-web-devicons",
-    disable = not lvim.use_icons,
-  },
+  -- {
+  --   "kyazdani42/nvim-web-devicons",
+  --   enabled = lvim.use_icons,
+  -- },
 
   -- Status Line and Bufferline
   {
@@ -184,7 +179,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.lualine").setup()
     end,
-    disable = not lvim.builtin.lualine.active,
+    enabled = lvim.builtin.lualine.active,
   },
 
   -- breadcrumbs
@@ -193,7 +188,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.breadcrumbs").setup()
     end,
-    disable = not lvim.builtin.breadcrumbs.active,
+    enabled = lvim.builtin.breadcrumbs.active,
   },
 
   {
@@ -202,8 +197,7 @@ local core_plugins = {
       require("lvim.core.bufferline").setup()
     end,
     branch = "main",
-    event = "BufWinEnter",
-    disable = not lvim.builtin.bufferline.active,
+    enabled = lvim.builtin.bufferline.active,
   },
 
   -- Debugging
@@ -213,7 +207,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.dap").setup()
     end,
-    disable = not lvim.builtin.dap.active,
+    enabled = lvim.builtin.dap.active,
   },
 
   -- Debugger user interface
@@ -222,7 +216,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.dap").setup_ui()
     end,
-    disable = not lvim.builtin.dap.active,
+    enabled = lvim.builtin.dap.active,
   },
 
   -- alpha
@@ -231,23 +225,24 @@ local core_plugins = {
     config = function()
       require("lvim.core.alpha").setup()
     end,
-    disable = not lvim.builtin.alpha.active,
+    enabled = lvim.builtin.alpha.active,
   },
 
   -- Terminal
   {
     "akinsho/toggleterm.nvim",
-    event = "BufWinEnter",
+    event = "VeryLazy",
     branch = "main",
     config = function()
       require("lvim.core.terminal").setup()
     end,
-    disable = not lvim.builtin.terminal.active,
+    enabled = lvim.builtin.terminal.active,
   },
 
   -- SchemaStore
   {
     "b0o/schemastore.nvim",
+    lazy = true,
   },
 
   {
@@ -255,7 +250,8 @@ local core_plugins = {
     config = function()
       require("lvim.core.illuminate").setup()
     end,
-    disable = not lvim.builtin.illuminate.active,
+    event = "VeryLazy",
+    enabled = lvim.builtin.illuminate.active,
   },
 
   {
@@ -263,7 +259,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.indentlines").setup()
     end,
-    disable = not lvim.builtin.indentlines.active,
+    enabled = lvim.builtin.indentlines.active,
   },
 
   {
@@ -277,7 +273,7 @@ local core_plugins = {
         end
       end)
     end,
-    disable = lvim.colorscheme ~= "onedarker",
+    enabled = lvim.colorscheme == "onedarker",
   },
 
   {
@@ -287,7 +283,7 @@ local core_plugins = {
         require("bigfile").config(lvim.builtin.bigfile.config)
       end)
     end,
-    disable = not lvim.builtin.bigfile.active,
+    enabled = lvim.builtin.bigfile.active,
   },
 
   {
@@ -295,7 +291,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.nvimsurround").setup()
     end,
-    disable = not lvim.builtin.nvimsurround.active,
+    enabled = lvim.builtin.nvimsurround.active,
   },
 
   {
@@ -303,11 +299,11 @@ local core_plugins = {
     config = function()
       require("lvim.core.noice").setup()
     end,
-    disable = not lvim.builtin.noice.active,
-    requires = {
+    enabled = lvim.builtin.noice.active,
+    dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      module = "nui"
+      lazy = true,
 
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
@@ -342,7 +338,7 @@ local core_plugins = {
     config = function()
       require("lvim.core.neoscroll").setup()
     end,
-    disable = not lvim.builtin.neoscroll.active
+    enabled = lvim.builtin.neoscroll.active
   },
 
   {
@@ -350,59 +346,68 @@ local core_plugins = {
     config = function()
       require("lvim.core.numb").setup()
     end,
-    disable = not lvim.builtin.numb.active
+    enabled = lvim.builtin.numb.active
   },
 
   {
     "Shatur/neovim-session-manager",
     config = function() require('lvim.core.sessionmanager').setup() end,
-    disable = not lvim.builtin.sessionmanager.active
+    ft = "alpha",
+    lazy = true,
+    enabled = lvim.builtin.sessionmanager.active
   },
 
   {
     "simrat39/symbols-outline.nvim",
     config = function() require('lvim.core.symbolsoutline').setup() end,
-    disable = not lvim.builtin.sessionmanager.active
+    enabled = lvim.builtin.sessionmanager.active
   },
 
   {
     "TimUntersberger/neogit",
-    requires = {
-      "sindrets/diffview.nvim",
-      config = function() require("lvim.core.diffview").setup() end,
-      disable = not lvim.builtin.diffview.active
-    },
+    --dependencies = {
+    -- "sindrets/diffview.nvim",
+    -- opts = require("lvim.core.diffview").config(),
+    -- config = function() require("lvim.core.diffview").setup() end,
+    -- enabled = false -- lvim.builtin.diffview.active
+    --},
     config = function() require("lvim.core.neogit").setup() end,
-    disable = not lvim.builtin.neogit.active
+    event = "VeryLazy",
+    enabled = lvim.builtin.neogit.active
   },
 
   {
     "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function() require("lvim.core.trouble").setup() end,
-    disable = not lvim.builtin.trouble.active
+    enabled = lvim.builtin.trouble.active
   },
 
   { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter-textobjects",
+
+    dependencies = { "nvim-treesitter" },
+    event = "VeryLazy"
   },
 
   { -- show and a navigate to an unde tree
     "mbbill/undotree",
     config = function() require('lvim.core.undotree').setup() end,
-    disable = not lvim.builtin.undotree.active
+    event = "VeryLazy",
+    enabled = lvim.builtin.undotree.active
   },
 
   { -- Intercept ui select entries with telescope
-    "nvim-telescope/telescope-ui-select.nvim"
+    "nvim-telescope/telescope-ui-select.nvim",
+    event = "VeryLazy",
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
     config = function() require('lvim.core.neotree').setup() end,
     branch = "v2.x",
-    disable = not lvim.builtin.neotree.active,
-    requires = {
+    enabled = lvim.builtin.neotree.active,
+    event = "VeryLazy",
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
@@ -411,29 +416,36 @@ local core_plugins = {
   {
     "NvChad/nvim-colorizer.lua",
     config = function() require("lvim.core.colorizer").setup() end,
-    disable = not lvim.builtin.colorizer.active,
+    enabled = lvim.builtin.colorizer.active,
   },
   {
     "catppuccin/nvim",
     config = function() require("lvim.core.catppuccin").setup() end,
-    as = "catppuccin",
-    disable = not lvim.builtin.catppuccin.acitve
+    name = "catppuccin",
+    enabled = lvim.builtin.catppuccin.acitve
   }
 
 }
 
 local default_snapshot_path = join_paths(get_lvim_base_dir(), "snapshots", "default.json")
 local content = vim.fn.readfile(default_snapshot_path)
-local default_sha1 = vim.fn.json_decode(content)
+local default_sha1 = assert(vim.fn.json_decode(content))
+
+-- taken form <https://github.com/folke/lazy.nvim/blob/c7122d64cdf16766433588486adcee67571de6d0/lua/lazy/core/plugin.lua#L27>
+local get_short_name = function(long_name)
+  local name = long_name:sub(-4) == ".git" and long_name:sub(1, -5) or long_name
+  local slash = name:reverse():find("/", 1, true) --[[@as number?]]
+  return slash and name:sub(#name - slash + 2) or long_name:gsub("%W+", "_")
+end
 
 local get_default_sha1 = function(spec)
-  local short_name, _ = require("packer.util").get_plugin_short_name(spec)
+  local short_name = get_short_name(spec[1])
   return default_sha1[short_name] and default_sha1[short_name].commit
 end
 
 if not vim.env.LVIM_DEV_MODE then
+  -- Manually lock the commit hash since Packer's snapshots are unreliable in headless mode
   for _, spec in ipairs(core_plugins) do
-    -- Manually lock the commit hash since Packer's snapshots are unreliable in headless mode
     spec["commit"] = get_default_sha1(spec)
   end
 end
